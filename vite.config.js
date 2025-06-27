82165114678185ea33b3728d17bb3955c74c6cc9
Tobias Bollinger <tobias.bollinger@bs.ch>
2025-06-27 11:30:23 +0200

    fix: update deployment workflow to install Slidev CLI and adjust build command; remove base path logic from Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [slidevClicks()],
-    base: process.env.NODE_ENV === 'production' 
-        ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/`
-        : '/',
 });
